b247d9f83f65701aba807b5b63f1bfb93c1a4f5e
delashafira <96773024+Fritadila@users.noreply.github.com>
2024-06-17 00:40:22 +0700

    start.sh

--- start.sh
+++ start.sh
@@ -5,14 +5,19 @@ export PYTHONUNBUFFERED=true
 VIRTUALENV=.data/venv
 
 if [ ! -d $VIRTUALENV ]; then
-    python3 -m venv $VIRTUALENV
+  python3 -m venv $VIRTUALENV
 fi
 
 if [ ! -f $VIRTUALENV/bin/pip ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
+# Print debug information
+echo "Python interpreter path: $VIRTUALENV/bin/python3"
+ls -l $VIRTUALENV/bin/python3  # Check if the interpreter exists
+
+# Install dependencies
 $VIRTUALENV/bin/pip install -r requirements.txt
 
+# Run the application
 $VIRTUALENV/bin/python3 app.py
-Footer
